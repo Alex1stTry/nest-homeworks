13f07a00df0bfcd446a87ec05283cddc9bf5aaf5
Alex1stTry <malyshevalex1994@gmail.com>
2024-05-23 21:49:12 +0300

    initial

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -18,7 +18,7 @@ export class UserController {
 
   @Post()
   public async create(@Body() createUserDto: CreateUserDto): Promise<any> {
-    return await this.userService.create(createUserDto);
+    return this.userService.create(createUserDto);
   }
 
   @Get()
